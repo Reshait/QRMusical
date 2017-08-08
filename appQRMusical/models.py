e8634f79cd339cbfb85dfe2458e9669682640329
Reshait <p22romat@uco.es>
2017-08-08 19:12:50 +0200

    Upload without File models

--- appQRMusical/models.py
+++ appQRMusical/models.py
@@ -18,9 +18,12 @@ class Settings(models.Model):
     image_rotation = models.BooleanField(default=False)
     timeout = models.IntegerField(default=5000)
 
+
 class File(models.Model):
     id = models.AutoField(primary_key=True)
     description = models.CharField(max_length=255, blank=True)
     file = models.FileField(upload_to='images/')
-#    file_type = models.
     upload_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id
--- appQRMusical/views.py
+++ appQRMusical/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
+from django.core.files.storage import FileSystemStorage
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import UpdateView
 from django.core.urlresolvers import reverse_lazy
 
-from .models import Settings
+from .models import Settings, File
 from .forms import FileForm
-
+from django.conf import settings
+import os
 # Create your views here.
 
 #def home(request):
@@ -40,7 +42,7 @@ def take_photo():
             ' -rot ' + str(global_settings.image_rotation) +
             ' -o ' + "captura.jpg" ])
     return
-
+"""
 def upload(request):
     if request.method == 'POST':
         form = FileForm(request.POST, request.FILES)
@@ -52,5 +54,26 @@ def upload(request):
     return render(request, 'upload.html', {
         'form': form
     })
+"""
 
+def upload(request):
+    output_list = os.popen('ls').read()
+    output_list = output_list.splitlines()
+
+    if request.method == 'POST' and request.FILES['myfile']:
+       
+        myfile = request.FILES['myfile']
+
+        if myfile.name.endswith('.mp3'):
+            uploaded_file_url = os.path.join(settings.MEDIA_ROOT, 'sounds/')
+        elif myfile.name.endswith('.jpg'):
+            uploaded_file_url = os.path.join(settings.MEDIA_ROOT, 'images/')
+        else:
+            uploaded_file_url = fs.url(filename)
+
+        fs = FileSystemStorage(location = uploaded_file_url)
+        filename = fs.save(myfile.name, myfile)
+
+        return render(request, 'upload.html', {'uploaded_file_url': uploaded_file_url, 'output_list': output_list})
+    return render(request, 'upload.html', {'output_list': output_list})
 
